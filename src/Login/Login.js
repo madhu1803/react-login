ae7ef13d9b695b923b42b0267da700d25fe2cf6a
madhumithaa <madhumithaa1411@gmail.com>
2020-07-14 15:32:45 +0530

    class component login changed

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Login = (props) => {
-  return (
-    <div className="pl-5 pr-5">
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
+class Login extends Component {
+  render() {
+    return (
+      <div className="pl-5 pr-5">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
